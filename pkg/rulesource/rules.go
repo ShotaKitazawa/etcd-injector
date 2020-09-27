623365bc77a300bd0e307ef53c7797050b0a3d25
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-27 10:42:46 +0900

    fix: detect "repl" type in rules.yaml

--- pkg/rulesource/rules.go
+++ pkg/rulesource/rules.go
@@ -1,6 +1,6 @@
 package rulesource
 
 type Rule struct {
-	JSONPath string `json:"jsonpath"`
-	Repl     string `json:"repl"`
+	JSONPath string      `json:"jsonpath"`
+	Repl     interface{} `json:"repl"`
 }
